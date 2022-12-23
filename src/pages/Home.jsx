6f80c650638043187969cab1650689da36335fe3
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-23 22:09:38 +0800

    Add card container & card header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import { Link } from 'react-router-dom'
+import CardContainer from '../components/CardContainer'
 
 const Home = () => {
-  return <div>Homepage</div>
+  const cards = [0, 1, 2, 3]
+  return (
+    <div className='w-full h-[95vh]'>
+      <div className='flex flex-row h-full flex-wrap justify-evenly p-5'>
+        {cards.map((card) => {
+          return <CardContainer />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Home
