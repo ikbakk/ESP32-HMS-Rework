18d13a0150d6a2ed36cf24dbea3c98748559f191
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-27 01:17:26 +0800

    Report table card added

--- src/components/ReportTable.jsx
+++ src/components/ReportTable.jsx
@@ -1,53 +1,51 @@
+import _ from 'lodash'
 import { HiOutlineDownload } from 'react-icons/hi'
+import {
+  FaHeartbeat,
+  FaLungs,
+  FaTemperatureHigh,
+  FaHistory
+} from 'react-icons/fa'
+import timeFormatter from '../config/timeFormatter'
 
-const ReportTable = () => {
+const ReportTable = ({ sensorId, data, maxRow }) => {
+  const id = _.toNumber(sensorId)
+  const nama = data.nama
+  const nilai = Object.values(data.nilai)
+
   return (
     <div className='report-wrapper'>
       <div className='flex w-full flex-row justify-center p-2'>
         <div className='flex flex-col text-center'>
-          <h2 className='text-2xl'>Ruang</h2>
-          <h3 className='text-xl'>Nama</h3>
+          <h2 className='text-2xl'>Ruang {id + 1}</h2>
+          <h3 className='text-xl'>{nama}</h3>
         </div>
       </div>
       <div className='m-1 flex w-full rounded-xl bg-secondary-focus'>
         <table className='text-center'>
           <tbody>
-            <tr className=''>
-              <th>ts</th>
-              <th>hr</th>
-              <th>spo</th>
-              <th>temp</th>
-            </tr>
             <tr>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
+              <th>
+                <FaHistory className='relative left-1/2' />
+              </th>
+              <th>
+                <FaHeartbeat className='relative left-1/2' />
+              </th>
+              <th>
+                <FaLungs className='relative left-1/2' />
+              </th>
+              <th>
+                <FaTemperatureHigh className='relative left-1/2' />
+              </th>
             </tr>
-            <tr>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-            </tr>
-            <tr>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-            </tr>
-            <tr>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-            </tr>
-            <tr>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-              <td className=' w-20'>a</td>
-            </tr>
+            {nilai.slice(Math.max(nilai.length - `${maxRow}`, 0)).map((n) => (
+              <tr>
+                <td className='w-20'>{timeFormatter(n.timestamp)}</td>
+                <td className='w-20'>{n.beat} bpm</td>
+                <td className='w-20'>{n.spo2} %</td>
+                <td className='w-20'>{n.temp} °C</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,12 +1,44 @@
+import { useDatabaseValue } from '@react-query-firebase/database'
+import { ref } from 'firebase/database'
+import { database } from '../config/firebase'
+
 import ReportTable from '../components/ReportTable'
+import Loading from './Loading'
 
 const History = () => {
+  const { data, isLoading } = useDatabaseValue(
+    ['userId'],
+    ref(database, `userId/`),
+    { subscribe: true, toArray: false },
+    {
+      keepPreviousData: true
+    }
+  )
   return (
-    <div className='home-wrapper'>
-      <div className='home-container'>
-        <ReportTable />
-      </div>
-    </div>
+    <>
+      {isLoading == true ? (
+        <Loading />
+      ) : (
+        <div className='home-wrapper'>
+          <div className='home-container'>
+            {data === null ? (
+              <CardEmpty />
+            ) : (
+              Object.entries(data).map((i) =>
+                i[1] == undefined ? null : (
+                  <ReportTable
+                    key={i[0]}
+                    sensorId={i[0]}
+                    data={i[1]}
+                    maxRow={5}
+                  />
+                )
+              )
+            )}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
